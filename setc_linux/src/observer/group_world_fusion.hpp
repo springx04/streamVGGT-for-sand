85e96c25940b58b36b1a1e41a16b125f1d259fe2
SpringX <springx04@users.noreply.github.com>
2026-08-29 12:40:00 +0800

    Filter world fusion by cross-view support

--- setc_linux/src/observer/group_world_fusion.hpp
+++ setc_linux/src/observer/group_world_fusion.hpp
@@ -74,6 +74,7 @@ private:
     float floor_band_ = 0.01f;
     float max_object_height_ = 1.0f;
     float global_color_gain_ = 1.0f;
+    std::uint64_t accepted_fuse_count_ = 0;
     // Indexed by model view, then RGB channel. View 1 is the fixed anchor.
     std::array<std::array<float, 3>, 3> color_gain_{{
         {{1.0f, 1.0f, 1.0f}},
